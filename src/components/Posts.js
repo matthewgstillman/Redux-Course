bb9102a9c09d19858d46c85d85fe48c5708704db
matthewgstillman <matthewgstillman@gmail.com>
2024-09-09 16:27:24 -0600

    latest update

---
+++ src/components/Posts.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
