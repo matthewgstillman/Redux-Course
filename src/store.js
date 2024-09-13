703ce8cd53a2394f94d620d3e378c4ffb45d0fbc
matthewgstillman <matthewgstillman@gmail.com>
2024-09-12 18:10:59 -0600

    reducers folder update

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
@@ -11,7 +11,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Enable Redux DevTools if available
   )
 );
 
