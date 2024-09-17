4288e327a6b2a156fbff92a95b7d2a000f919087
matthewgstillman <matthewgstillman@gmail.com>
2024-09-16 20:28:30 -0600

    latest updates

--- src/store.js
+++ src/store.js
@@ -9,10 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Enable Redux DevTools if available
-  )
+  applyMiddleware(...middleware)
 );
 
 export default store;
